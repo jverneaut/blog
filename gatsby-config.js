b4ee2ace62139b1f0b1b0f3351942d0da78b62c6
jverneaut <jverneaut@gmail.com>
2019-08-17 10:51:41 +0200

    Load correct font weight

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Lato\:400:600`, `Roboto Mono`],
+        fonts: [`Lato\:400:700`, `Roboto Mono`],
         display: "swap",
       },
     },
