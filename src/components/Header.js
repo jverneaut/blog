71c53100788d151b5ac607af6dda87afb896e9f8
Julien Verneaut <jverneaut@gmail.com>
2021-04-02 15:55:38 +0200

    Change header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import profile from '../../assets/julien-verneaut.jpg';
-
 const Header = () => (
   <div className="header">
     <div className="container">
       <Link to="/" className="logo">
-        <img src={profile} alt="Julien Verneaut" />
-        <span>Blog – Julien Verneaut</span>
+        <span>
+          <strong>Blog</strong> – Julien Verneaut
+        </span>
       </Link>
     </div>
   </div>
