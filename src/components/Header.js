706427c03321b4a50ad4adeaa6c92415b6cd162e
Julien Verneaut <jverneaut@gmail.com>
2021-04-02 15:46:28 +0200

    Re-style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,10 @@ import profile from '../../assets/julien-verneaut.jpg';
 
 const Header = () => (
   <div className="header">
-    <Link to="/" className="logo">
-      <img src={profile} alt="Julien Verneaut" />
-      Julien Verneaut
-    </Link>
-    <div className="nav">
-      <Link className="nav__item" to="/">
-        Archive
+    <div className="container">
+      <Link to="/" className="logo">
+        <img src={profile} alt="Julien Verneaut" />
+        <span>Blog – Julien Verneaut</span>
       </Link>
     </div>
   </div>
